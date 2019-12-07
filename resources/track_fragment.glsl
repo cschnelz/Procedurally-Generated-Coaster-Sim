47ea311ea06ca68cb697398d3cbeace5801d7751
Charles Schnelz <48392865+cschnelz@users.noreply.github.com>
2019-12-07 12:56:20 -0800

    New cube normals

--- resources/track_fragment.glsl
+++ resources/track_fragment.glsl
@@ -3,33 +3,32 @@ out vec4 color;
 in vec3 vertex_normal;
 in vec3 vertex_pos;
 in vec2 vertex_tex;
-//uniform vec3 campos;
 
-uniform float color_change;
+uniform vec3 campos;
+uniform vec3 color_change;
+uniform vec3 light_pos;
 //uniform sampler2D tex;
 //uniform sampler2D tex2;
 
 void main()
 {
-//vec3 n = normalize(vertex_normal);
-//vec3 lp=vec3(10,-20,-100);
-//vec3 ld = normalize(vertex_pos - lp);
-//float diffuse = dot(n,ld);
+vec3 n = normalize(vertex_normal);
+vec3 lp=vec3(0,50,0);
+vec3 ld = normalize(light_pos - vertex_pos);
+float diffuse = dot(n,ld);
 
-//color.rgb = texture(tex, vertex_tex).rgb;
-
-color.rgb = vertex_normal;
+color.rgb = color_change + .1;
 color.a = 1.f;
+color.rgb *= diffuse*0.7;
 
-//color.rgb *= diffuse*0.7;
+vec3 cam = vec3(-campos.x, -campos.y, -campos.z);
+vec3 cd = normalize(vertex_pos - cam);
+vec3 h = normalize(cd+ld);
+float spec = dot(n,h);
+spec = clamp(spec,0,1);
+spec = pow(spec,20);
+color.rgb += vec3(1,1,1)*spec*3;
+color.a=1;
 
-//vec3 cd = normalize(vertex_pos - campos);
-//vec3 h = normalize(cd+ld);
-//float spec = dot(n,h);
-//spec = clamp(spec,0,1);
-//spec = pow(spec,20);
-//color.rgb += vec3(1,1,1)*spec*3;
-//color.a=1;
-//
 
 }
