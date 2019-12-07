90887a4d9e366d76ed5c6c3d061a9fd282314977
Charles Schnelz <48392865+cschnelz@users.noreply.github.com>
2019-12-06 18:42:06 -0800

    removed borders

--- src/main.cpp
+++ src/main.cpp
@@ -920,44 +920,6 @@ public:
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, Texture);
 
-		// this code will be removed in a later stage, used to create a visual of the border that bounds the walking algorithm
-		glm::mat4 scaleBorder = glm::scale(glm::mat4(1), vec3(.3, .3, .3));
-		glm::mat4 feauxBorder = glm::translate(glm::mat4(1), vec3(border, 0, 0));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(-border, 0, 0));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(0, 0, -border));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(0, 0, border));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(-border, 0, -border));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(-border, 0, border));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(border, 0, border));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-		feauxBorder = glm::translate(glm::mat4(1), vec3(border, 0, -border));
-		M = feauxBorder * scaleBorder;
-		glUniformMatrix4fv(prog->getUniform("M"), 1, GL_FALSE, &M[0][0]);
-		shape->draw(prog, FALSE);
-
-
-
-
 		heightshader->bind();
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 		glm::mat4 TransY = glm::translate(glm::mat4(1.0f), glm::vec3(-50.0f, -3.0f, -50));
